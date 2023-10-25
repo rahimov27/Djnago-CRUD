00b2884fa6f4e6b58dca4464278355295778da93
rahimov27 <89564054+rahimov27@users.noreply.github.com>
2023-10-25 15:09:49 +0200

    Delete thirdproject directory

--- thirdproject/todoapp/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-from . import views
-
-urlpatterns = [
-    path("/", views.index, name="index"),
-    path("", views.list_view, name="list_view"),
-]
